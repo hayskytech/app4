55939e37f11f34b94ebc71b9a8e6b6abfc3c764b
Sufyan Shaik <mdsufyan7@gmail.com>
2023-11-22 10:16:42 +0530

    states

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from 'react'
-import { About, Contact } from './pages/About'
-import Waa from './pages/Waa'
-import Header from './pages/Header'
+import States from './pages/States'
 
 export default function App() {
   return (
     <div>
-      <Header />
 
-      <h1 style={{ color: 'red', backgroundColor: 'skyblue' }}>Hello world</h1>
-      <table border='1'>
-        <tr>
-          <td>hai</td>
-          <td>hai</td>
-          <td>hai</td>
-        </tr>
-        <tr>
-          <td>hai</td>
-          <td>hai</td>
-          <td>hai</td>
-        </tr>
-        <tr>
-          <td>hai</td>
-          <td>hai</td>
-          <td>hai</td>
-        </tr>
-      </table>
+      <States />
 
-      <About />
-      <Contact />
-      <Waa />
     </div>
   )
 }
