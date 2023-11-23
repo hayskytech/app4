1b41fb5da52e47c0192766c4e3602c381696c4e9
Sufyan Shaik <mdsufyan7@gmail.com>
2023-11-23 10:16:46 +0530

    router & css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react'
+import 'semantic-ui-css/semantic.min.css'
 import States from './pages/States'
+import Waa from './pages/Waa'
+import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom'
+import { About, Contact } from './pages/About'
+import NoPage from './pages/NoPage'
+import { Button } from 'semantic-ui-react'
 
 export default function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainMenu />}>
+            <Route path="" element={<Waa />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="states" element={<States />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+function MainMenu() {
+  return (
+    <div>
 
-      <States />
+      <Link to="/">
+        <Button color='blue'>Home</Button>
+      </Link>
 
+      <Button as={Link} to="/about" color='blue'>About</Button>
+      <Button as={Link} to="/contact" color='blue'>Contact</Button>
+      <Button as={Link} to="/states" color='blue'>States</Button>
+      <Button as={Link} to='/user' color='blue'>User</Button>
+
+      <hr />
+      <div style={{ padding: 10 }}>
+        <Outlet />
+      </div>
+      <hr />
     </div>
   )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,47 @@
 import React from 'react'
-
+import './styles.css'
 export function About() {
+
+  const ss = { color: 'red', backgroundColor: 'skyblue' }
+
   return (
     <div>
-      this is about page
+      <h2
+        style={{
+          color: 'red',
+          backgroundColor: 'skyblue',
+          borderColor: 'black',
+          borderWidth: 2,
+          borderStyle: 'solid'
+        }}
+      >Hello world</h2>
+
+      <h3 style={ss}>ksadb iasdu</h3>
+      <h3 style={ss}>ksadb iasdu</h3>
+      <h3 style={ss}>ksadb iasdu</h3>
+      <h3 style={ss}>ksadb iasdu</h3>
+      <h3 style={ss}>ksadb iasdu</h3>
+
+      <table>
+        <tr>
+          <td>jasd asd</td>
+          <td>jasd asd</td>
+          <td>jasd asd</td>
+          <td>jasd asd</td>
+        </tr>
+        <tr>
+          <td>jasd asd</td>
+          <td>jasd asd</td>
+          <td>jasd asd</td>
+          <td>jasd asd</td>
+        </tr>
+        <tr>
+          <td>jasd asd</td>
+          <td>jasd asd</td>
+          <td>jasd asd</td>
+          <td>jasd asd</td>
+        </tr>
+      </table>
     </div>
   )
 }
@@ -12,7 +50,8 @@ export function About() {
 export function Contact() {
   return (
     <div>
-      this is contact page
+      <h2>Contact</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus blanditiis quis placeat dolor esse similique, a saepe excepturi optio quae consequuntur cumque enim assumenda adipisci incidunt nihil! Maiores, incidunt facere. Laudantium labore velit delectus, cum laboriosam mollitia animi adipisci enim sint eligendi esse voluptatibus pariatur aliquam ipsum error repellat perspiciatis possimus non magni aliquid natus quaerat debitis sunt. Nisi tempora dicta corporis qui non vitae earum explicabo quas maxime. Doloribus aliquam illo perspiciatis exercitationem, eligendi a laudantium voluptas quidem modi quibusdam iure quos velit, harum maxime, explicabo nulla ab sit. Eveniet ea incidunt earum ad reiciendis, corrupti animi iusto pariatur nemo provident culpa temporibus? Fugit ad cupiditate eum obcaecati optio, atque et quisquam. Ipsum quas ullam libero quasi labore, ratione repudiandae, dolor blanditiis, tenetur culpa accusamus. Eligendi inventore magnam placeat numquam distinctio debitis, nisi odit soluta asperiores nemo exercitationem aliquam optio est voluptates dolores animi nihil fuga eum. Quam perferendis recusandae, asperiores illo, voluptatum sunt cumque adipisci incidunt aut nobis deserunt aspernatur placeat optio vel consequuntur sit doloremque fuga nisi dolores veniam accusantium culpa quia! Suscipit maxime illo beatae quo consectetur aspernatur reprehenderit! Dolorem quidem cupiditate illo sed temporibus aliquid culpa magni unde deleniti, sunt, nemo molestiae dolores tempore? Dolore.</p>
     </div>
   )
 }
