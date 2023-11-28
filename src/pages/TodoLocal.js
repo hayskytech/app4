01c8f683591cc1e552e5b4545310c2cc02e6c89f
Sufyan Shaik <mdsufyan7@gmail.com>
2023-11-28 10:16:22 +0530

    using semantic

--- src/pages/TodoLocal.js
+++ src/pages/TodoLocal.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import { Button, Input, List } from 'semantic-ui-react'
 
 export default function TodoLocal() {
   let oldData = []
@@ -15,40 +15,57 @@ export default function TodoLocal() {
     if (text === '') return
     const newList = list.concat(text)
     setList(newList)
-    localStorage.setItem('myData', JSON.stringify(newList))
     setText('')
   }
 
-  function deleteAll() {
+  function deleteAll(e) {
+    e.preventDefault()
     const newList = []
     setList(newList)
-    localStorage.setItem('myData', JSON.stringify(newList))
   }
+
   function deleteOne(index) {
     let newList = [...list]
     newList.splice(index, 1)
     setList(newList)
-    localStorage.setItem('myData', JSON.stringify(newList))
   }
+
+  useEffect(() => {
+    localStorage.setItem('myData', JSON.stringify(list))
+  }, [list])
 
   return (
     <div>
+
       <form onSubmit={addItem}>
         <Input type="text"
+          placeholder='Todo Item:'
           value={text}
           onChange={(e) => { setText(e.target.value) }}
-        />
-        <Button color='green'>ADD</Button>
+        />{` `}
+        <Button color='green' icon='plus'></Button>
+        <Button color='red' onClick={deleteAll}>Clear All</Button>
       </form>
-      <Button color='red' onClick={deleteAll}>Delete All</Button>
 
-      {list.map((item, index) => {
-        return (<li key={index}>
-          {item}
-          <button onClick={() => deleteOne(index)}>x</button>
-        </li>)
-      })}
+      <List divided verticalAlign='middle'>
 
+        {list.map((item, index) => {
+          return (
+            <List.Item key={index}>
+              <List.Content floated='right'>
+                <Button
+                  color='red'
+                  icon='close'
+                  onClick={() => deleteOne(index)}
+                ></Button>
+              </List.Content>
+              <List.Content>
+                <List.Header>{item}</List.Header>
+              </List.Content>
+            </List.Item>)
+        })}
+
+      </List>
     </div>
   )
 }
