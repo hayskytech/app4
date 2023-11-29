7e802e155a6956ce37ebecf37b77901db0e568fd
Sufyan Shaik <mdsufyan7@gmail.com>
2023-11-29 10:33:01 +0530

    api1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import States from './pages/States'
-import Waa from './pages/Waa'
 import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom'
 import { About, Contact } from './pages/About'
 import NoPage from './pages/NoPage'
 import { Button } from 'semantic-ui-react'
 import TodoListSimple from './pages/TodoListSimple'
 import TodoLocal from './pages/TodoLocal'
+import CatFacts from './api/CatFacts'
 
 export default function App() {
   return (
@@ -15,12 +15,12 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainMenu />}>
-            <Route path="" element={<Waa />} />
-            <Route path="about" element={<About />} />
+            <Route index element={<About />} />
             <Route path="contact" element={<Contact />} />
             <Route path="todolistsimple" element={<TodoListSimple />} />
             <Route path="states" element={<States />} />
             <Route path="todolocal" element={<TodoLocal />} />
+            <Route path="catfacts" element={<CatFacts />} />
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
@@ -32,11 +32,11 @@ function MainMenu() {
   return (
     <div>
       <Button as={Link} to='/' color='blue'>Home</Button>
-      <Button as={Link} to="/about" color='blue'>About</Button>
       <Button as={Link} to="/contact" color='blue'>Contact</Button>
       <Button as={Link} to="/states" color='blue'>States</Button>
       <Button as={Link} to='/todolistsimple' color='blue'>TodoListSimple</Button>
       <Button as={Link} to='/todolocal' color='blue'>TodoLocal</Button>
+      <Button as={Link} to='/catfacts' color='blue'>CatFacts</Button>
       <hr />
       <div style={{ padding: 10 }}>
         <Outlet />
--- src/pages/Waa.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Waa() {
-    return (
-        <div>
-            this is waa page
-        </div>
-    )
-}
